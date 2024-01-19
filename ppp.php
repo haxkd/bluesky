2db977f93de149c8cc9727178546ebf5b6514ae4
Abul Hasan <Abulhax@gmail.com>
2024-01-19 22:41:43 +0530

    Update ppp.php

--- ppp.php
+++ ppp.php
@@ -6,16 +6,12 @@
                        <div class="panel-body">
                       <div class="col-md-4 col-xs-12 col-sm-6 col-lg-4">
                        <img alt="<?php echo $row['fname']." ".$row['lname']; ?>" src="/images/user/<?php echo $row['id']; ?>/<?php echo $row['image']; ?>" id="profile-image1"  class=" img-circle img-responsive"> 
-                     
-                 
                       </div>
                       <div class="col-md-8 col-xs-12 col-sm-6 col-lg-8" >
                           <div class="container" >
                             <h2><?php echo strtoupper($row['fname']." ".$row['lname']); ?></h2>
                             <p>   <b> </b></p>
-                          
-                           
-                          </div>
+                            </div>
                            <hr>
                           <ul class="container details" >
                             <li><p><span class="glyphicon glyphicon-user one" style="width:50px;"></span><mark><?php echo $row['user_name']; ?></mark></p></li>
@@ -26,13 +22,9 @@
                           <hr>
                           <div class="col-sm-5 col-xs-6 tital " ><span class="glyphicon glyphicon-calendar" style="width:50px;"></span><?php echo $row['dob']; ?></div>
                           <div class="col-sm-5 col-xs-6 tital " ><i class="fa fa-flag" aria-hidden="true" style="width:50px;"></i><?php echo $row['country']; ?></div>
-                          
                           <div class="col-sm-5 col-xs-6 tital " ><i class="fa fa-address-card-o" aria-hidden="true" style="width:50px;"></i><?php echo $row['address']; ?> <?php echo $row['pincode']; ?></div>
-                          
                       </div>
                 </div>
             </div>
-            
             </div>
-            
             </div>
