ea03472f13c5f1f3b7d4f9f971226463374f84b9
Abul Hasan <Abulhax@gmail.com>
2024-01-19 22:39:40 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,23 +1,18 @@
 <?php
-
     $to = "abulhax@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
-
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $from . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
     $subject = "You have a message from your Bitmap Photography.";
-
     $logo = 'img/logo.png';
     $link = '#';
-
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
@@ -31,14 +26,10 @@
 	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
-
     $send = mail($to, $subject, $body, $headers);
-
 if(!$send) {   
      echo "Error";   
 } else {
     echo "Success";
 }
-
-
 ?>
