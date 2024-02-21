24785324effc56eddd7753e9e1e8c98e2ead91b5
Abul Hasan <Abulhax@gmail.com>
2024-02-21 22:40:55 +0530

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -14,8 +14,7 @@ if(isset($_SESSION['admin'])){
         $user = $_POST['username'];
         $password = $_POST['password'];
         $sql_query = "SELECT * FROM `admin_login` WHERE `username`='$user'";
-		$result=mysqli_query($conn,$sql_query);
-       
+	$result=mysqli_query($conn,$sql_query);
 	if(mysqli_num_rows($result)>0)
 	{
 		echo 'soething get';
@@ -43,9 +42,7 @@ if(isset($_SESSION['admin'])){
                  }else{
                  $msg= "";
                  }
-?>
-    
-    
+?>    
 <html>
 <head>
 	<title>Admin Login Bluesky</title>
@@ -55,13 +52,10 @@ if(isset($_SESSION['admin'])){
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
-
 	<!--Bootsrap 4 CDN-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
     <!--Fontawesome CDN-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-
 	<!--Custom styles-->
 </head>
 <style>
@@ -69,7 +63,6 @@ blink {
   -webkit-animation: 1s linear infinite condemned_blink_effect; /* for Safari 4.0 - 8.0 */
   animation: 1s linear infinite condemned_blink_effect;
 }
-
 /* for Safari 4.0 - 8.0 */
 @-webkit-keyframes condemned_blink_effect { 
   0% {
@@ -82,7 +75,6 @@ blink {
     visibility: visible;
   }
 }
-
 @keyframes condemned_blink_effect {
   0% {
     visibility: hidden;
@@ -95,9 +87,7 @@ blink {
   }
 }
 /* Made with love by Mutiullah Samim*/
-
 @import url('https://fonts.googleapis.com/css?family=Numans');
-
 html,body{
 background-image: url('http://getwallpapers.com/wallpaper/full/a/5/d/544750.jpg');
 background-size: cover;
@@ -105,12 +95,10 @@ background-repeat: no-repeat;
 height: 100%;
 font-family: 'Numans', sans-serif;
 }
-
 .container{
 height: 100%;
 align-content: center;
 }
-
 .card{
 height: 370px;
 margin-top: auto;
@@ -118,45 +106,36 @@ margin-bottom: auto;
 width: 400px;
 background-color: rgba(0,0,0,0.5) !important;
 }
-
 .social_icon span{
 font-size: 60px;
 margin-left: 10px;
 color: #FFC312;
 }
-
 .social_icon span:hover{
 color: white;
 cursor: pointer;
 }
-
 .card-header h3{
 color: white;
 }
-
 .social_icon{
 position: absolute;
 right: 20px;
 top: -45px;
 }
-
 .input-group-prepend span{
 width: 50px;
 background-color: #FFC312;
 color: black;
 border:0 !important;
 }
-
 input:focus{
 outline: 0 0 0 0  !important;
 box-shadow: 0 0 0 0 !important;
-
 }
-
 .remember{
 color: white;
 }
-
 .remember input
 {
 width: 20px;
@@ -164,37 +143,23 @@ height: 20px;
 margin-left: 15px;
 margin-right: 5px;
 }
-
 .login_btn{
 color: black;
 background-color: #FFC312;
 width: 100px;
 }
-
 .login_btn:hover{
 color: black;
 background-color: white;
 }
-
 .links{
 color: white;
 }
-
 .links a{
 margin-left: 4px;
 }
 </style>
 <body>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 <div class="container">
 	<div class="d-flex justify-content-center h-100">
 		<div class="card">
@@ -208,7 +173,6 @@ margin-left: 4px;
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
 						<input type="text" class="form-control" name="username" placeholder="username" required>
-						
 					</div>
 					<br/>
 					<div class="input-group form-group">
@@ -222,11 +186,9 @@ margin-left: 4px;
 						<input type="submit" value="Login" name="adminlogin" class="btn float-right login_btn">
 					</div>
 				</form>
-				
 			</div>
 			<div class="card-footer">
 			</div>
-			
 		</div>
 	</div>
 </div>
