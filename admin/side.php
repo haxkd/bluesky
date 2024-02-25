fb5c9d1a1b030745d35aebee6e722303f8b7fb6d
Abul Hasan <Abulhax@gmail.com>
2024-02-25 22:33:40 +0530

    Update side.php

--- admin/side.php
+++ admin/side.php
@@ -9,18 +9,13 @@ if (isset($_GET['p'])) {
 $selected="";
 $selected1="";
 }
-
-
 ?>
-
-<nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top" id="mainNav">
-    
+<nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top" id="mainNav">  
     <a class="navbar-brand" href="#">Admin Of BLUESKY</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
-    
         <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="AdminHome">
                 <a class="nav-link" href="index.php">
@@ -28,21 +23,18 @@ $selected1="";
                     <span class="nav-link-text"> Home Admin</span>
                 </a>
             </li>
-
              <li class="nav-item " data-toggle="tooltip" data-placement="right"  title="AllUser" >
                 <a class="nav-link <?php echo $selected ?>" href="all_user.php?p=all_user">
                     <i class="fa fa-fw fa-dashboard"></i>
                     <span class="nav-link-text">All User</span>
                 </a>
             </li>
-            
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="AllSeller">
                 <a class="nav-link <?php echo $selected1 ?>" href="all_seller.php?p=all_seller">
                     <i class="fa fa-fw fa-area-chart"></i>
                     <span class="nav-link-text">All seller</span>
                 </a>
             </li>
-             
              <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Property">
                 <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
                     <i class="fa fa-fw fa-file"></i>
@@ -52,7 +44,6 @@ $selected1="";
                     <li>
                         <a href="property.php">All Property</a>
                     </li>
-                    
                     <li>
                         <a href="#Avilable">Available</a>
                     </li>
@@ -60,18 +51,14 @@ $selected1="";
                     <li>
                         <a href="property.php">Unavailable</a>
                     </li>
-                  
                 </ul>
-                
             </li>
-            
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Feedback">
                 <a class="nav-link" href="feedback.php">
                     <i class="fa fa-fw fa-user"></i>
                     <span class="nav-link-text"> Feedbacks</span>
                 </a>
             </li>
-
         </ul>
         <ul class="navbar-nav sidenav-toggler ">
             <li class="nav-item">
@@ -79,14 +66,12 @@ $selected1="";
                     <i class="fa fa-fw fa-angle-left"></i>
                 </a>
             </li>
-        </ul>
-       
+        </ul>       
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a class="nav-link" href="logout.php">
                     <i class="fa fa-fw fa-sign-out"></i>Logout</a>
             </li>
         </ul>
-           
     </div>
 </nav>
